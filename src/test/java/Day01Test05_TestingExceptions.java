9fe8a0dd10dd5b926996a6b75b88f24709a9545d
Mehmet Emin Çatal <mhmtmnctl20@gmail.com>
2022-09-28 21:28:10 +0300

    28.09.2022

---
+++ src/test/java/Day01Test05_TestingExceptions.java
@@ -0,0 +1,48 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class Day01Test05_TestingExceptions {
+    //test ettiğimiz metodun, bekediğimiz exception fırlattı mı?
+
+    @Test
+    public void testException(){//nullPointer fırlatmadığı için kaldı
+        String str = "Merhaba Dünya";
+        assertThrows(NullPointerException.class,()->{//ikinci parametre olarak lambda verdik
+            System.out.println("testException is run");
+            str.length();//içinde bişey olsun diye yazdık. amaç içinde bişeyler olsun
+        });
+    }
+
+    //bölme işlemi için exception kontrolü
+    @Test
+    public void testException2(){//aritmetik exception fırlatacağı için test geçiyor
+        int sayi1=1;
+        int sayi2=0;
+        //exception'ın parentı da yazılabilir. : RunTimeException
+        assertThrows(ArithmeticException.class,()->{
+            System.out.println(sayi1/sayi2);
+        });
+    }
+
+    //yukardaki soruyu method ekleyerek yapalım
+    @Test
+    void testException3(){//aritmetik exception fırlattığı için geçti
+        int sayi1=1;
+        int sayi2=0;
+        assertThrows(ArithmeticException.class,()->{//aşağıdaki divide metodunu test edelm
+            divide(sayi1,sayi2);
+        });
+
+    }
+    private int divide (int a,int b){//bu metodu yukardaki 3 te kullancaz. bunu test etcez
+
+        return a/b;
+    }
+
+
+
+
+
+
+}
---
+++ src/test/java/Day02Test01_DisplayName.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class Day02Test01_DisplayName {
+
+    @Test
+    @DisplayName("test parseInt() to chek if it is throwing for non-digit value")//metot ismini uzun yazmaktansa bu annotation ile açıklama yazabiliriz ve çıktıda bu görünür
+    void testException(){//bu isim genel oldu. test_parseIn_if_it_is_throwing...bla bla şeklinde isim koymak lazım  bu da zor
+        String str = "Merhaba";//int'e çevirmeye çalışırsak numberFormatException atar
+        assertThrows(NumberFormatException.class,()->{Integer.parseInt(str);});//dolayısı ile test geçer
+    }
+
+
+}
